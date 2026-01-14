1e3ca6ac6ad56dd074f3c58a2f8ea5f35053fa51
root <11646@holbertonstudents.com>
2026-01-14 07:23:24 +0000

    p.alphabet

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-print("".join(map(lambda i: chr(i) if i not in (101, 113) else "", range(97, 123))))
+for alpha_letters in range(ord('a'), ord('z')+1):
+    if alpha_letters == 'e' or alpha_letters == 'q':
+       continue
+    print("{:c}".format(alpha_letters), end="")
