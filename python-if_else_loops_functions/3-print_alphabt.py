bbf2b78d9ddf54c6a7b9495cc9c4458cd9625ada
root <11646@holbertonstudents.com>
2026-01-14 07:03:26 +0000

    p.alphabet

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-print("{}".format("".join(map(lambda i: chr(i) if i not in (101, 113) else "", range(97, 123)))))
-
+print("".join(map(lambda i: chr(i) if i not in (101, 113) else "", range(97, 123))))
