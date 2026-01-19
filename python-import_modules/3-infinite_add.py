2d93fc67d879f43b4e5f5cca436e55b5bbd75194
11646-source <11646@holbertonstudents.com>
2026-01-19 06:14:15 +0000

    infinite

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import sys
 def main():
+    if len(sys.argv) <= 1:
+        print(0)
+        return
     total = 0
     for arg in sys.argv[1:]:
         total += int(arg)
-        print(total)
-        if __name__ == "__main__":
-            main()
+    print(total)
+if __name__ == "__main__":
+    main()
