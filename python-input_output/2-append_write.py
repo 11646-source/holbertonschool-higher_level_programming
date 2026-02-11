fe3ee04c535e867947624dc1379576b445ef5a2f
Ryan364412 <bundhoor50@gmail.com>
2026-02-11 11:07:04 +0400

    append

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that defines a function to append a UTF-8 text file and print its content."""
+def append_write(filename="", text=""):
+    """writes a string that append to a UTF-8 text and returns the number of characters written."""
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
+
