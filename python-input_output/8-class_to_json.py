31384bf84fc5f326afb318fb7b2d2381097b5351
Ryan364412 <bundhoor50@gmail.com>
2026-02-13 11:38:03 +0400

    jsCLass

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+Module that defines a function class_to_json.
+This function returns the dictionary description
+of an object for JSON serialization.
+"""
+
+
 def class_to_json(obj):
     """
     Returns the dictionary description of an object
