25391ee4034f17c2a439a855c3ad8e7095bbee51
Ryan364412 <bundhoor50@gmail.com>
2026-02-14 10:58:45 +0400

    N.student

---
+++ python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module that defines a Student class.
+"""
+
+
+class Student:
+    """
+    Defines a student with first name, last name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a new Student instance.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieve a dictionary representation of the Student instance.
+        If attrs is a list of strings, only attributes with names in
+        this list are included. Otherwise, all attributes are returned.
+        """
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            return {a: getattr(self, a) for a in attrs if hasattr(self, a)}
+        return self.__dict__
