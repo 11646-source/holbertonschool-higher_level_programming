4b3f3d29470c1e84c9a6e61c1bdce0f68ef66baf
Ryan364412 <bundhoor50@gmail.com>
2026-02-18 23:25:55 +0400

    m

---
+++ python-import_modules/5-variable_load.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Program that imports variable a from variable_load_5.py and prints its value."""
+
+from variable_load_5 import a
+
+
+if __name__ == "__main__":
+    print(a)
