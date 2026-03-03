2eaa7b8ec64419e2f4e8ee96f8ad2330882234c5
Ryan364412 <bundhoor50@gmail.com>
2026-03-03 11:00:10 +0400

    F.T id name

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- Script to insert a new row into first_table
--- New row: id = 89, name = 'Best school'
+-- New row: id = 89, name = 'Best School'
 
-INSERT INTO first_table (id, name) VALUES (89, 'Best school');
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
