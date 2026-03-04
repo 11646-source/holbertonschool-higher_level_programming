a43c838aadb45d149fd892272375d6d6d4eb1427
Ryan364412 <bundhoor50@gmail.com>
2026-03-04 16:07:58 +0400

    B.score

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,4 +1,8 @@
--- Select all records where the score is >= 10
+-- List all records with score >= 10
+-- from the table second_table
+-- Results show score then name, ordered by score descending
+
 SELECT score, name
-FROM hbtn_0c_0.second_table
-WHERE score >= 10;
+FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
