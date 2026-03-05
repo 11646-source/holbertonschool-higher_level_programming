5547bcf0b931ffe442f4466dfa352b253ac57c80
Ryan364412 <bundhoor50@gmail.com>
2026-03-05 20:40:47 +0400

    C.t name

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,6 +1,5 @@
 -- Create a table named id_not_null with an id column that cannot be NULL
 CREATE TABLE id_not_null (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, -- Unique identifier, always required
-    name VARCHAR(100),                          -- Optional name field
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- Automatically stores creation time
+    id INT NOT NULL,          -- id must always have a value, but duplicates are allowed
+    name VARCHAR(255) NOT NULL -- name cannot be NULL either
 );
