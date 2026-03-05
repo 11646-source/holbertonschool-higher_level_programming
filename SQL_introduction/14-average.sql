02d9ad4cb5bc47350b9ba5f9848df94a3241f42b
Ryan364412 <bundhoor50@gmail.com>
2026-03-05 09:38:12 +0400

    averege.S

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,7 +1,2 @@
--- Switch to the hbtn_0c_0 database
-USE hbtn_0c_0;
-
--- Compute the average of the 'score' column in the 'second_table' table
--- The AVG() function calculates the mean value of the specified numeric column
-SELECT AVG(score) AS average_score
-FROM second_table;
+-- Script to compute the average score from second_table
+SELECT AVG(score) AS average FROM second_table;
