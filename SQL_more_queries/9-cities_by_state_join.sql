f5ab401139bbf23f79f57ff5e3a007e9cb1d581b
Ryan364412 <bundhoor50@gmail.com>
2026-03-07 21:17:14 +0400

    join C&S

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,8 @@
--- Lists all cities of california from the database htbn_0d_usa
--- Usage: mysql -u root -p <database_name> < script.sql
+-- Lists all cities contained in the database hbtn_0d_usa
+-- Each record shows: cities.id - cities.name - states.name
+-- Results are sorted in ascending order by cities.id
 
-SELECT cities.id, cities.name
+SELECT cities.id, cities.name, states.name
 FROM cities, states
 WHERE cities.state_id = states.id
 ORDER BY cities.id ASC;
