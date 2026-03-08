f5cf364b6ecba4e2d149c57ad68b38d0f500472c
Ryan364412 <bundhoor50@gmail.com>
2026-03-08 09:52:30 +0400

    TV.S-GEN

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,8 +1,10 @@
--- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+-- Lists all shows contained in hbtn_0d_tvshows with their genre_id
 -- Each record shows: tv_shows.title - tv_show_genres.genre_id
--- Results are sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- Includes shows with no genre (NULL)
+-- Results are sorted by tv_shows.title and tv_show_genres.genre_id
 
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows, tv_show_genres
-WHERE tv_shows.id = tv_show_genres.show_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
