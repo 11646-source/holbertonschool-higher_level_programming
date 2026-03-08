2e55d41f4edbd02fe2c1edca35b643400823144a
Ryan364412 <bundhoor50@gmail.com>
2026-03-08 17:00:50 +0400

    S.gen

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- Lists all genres of the show Dexter
 -- Usage: mysql -u root -p <database_name> < 14-my_genres.sql
 
-SELECT g.name AS genre
+SELECT g.name AS name
 FROM tv_genres g
 JOIN tv_show_genres sg ON g.id = sg.genre_id
 JOIN tv_shows s ON s.id = sg.show_id
