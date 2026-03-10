a5126ecc95d7f1135d6730d4446cb584e73bcf02
Ryan364412 <bundhoor50@gmail.com>
2026-03-10 11:08:40 +0400

    cities.S

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,38 +1,38 @@
 #!/usr/bin/python3
 """
-Lists all cities from database htbn_0e_4_usa.
-Arguments: mysql username, mysql password, database name.
+Lists all cities from the database hbtn_0e_4_usa.
+Takes 3 arguments: mysql username, mysql password, and database name.
 """
 
-import MYSQLdb
+import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Get arguments
+    # Collect arguments
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-    state_name = sys.argv[4]
 
-    # conect to MySQL
+    # Connect to MySQL server
     db = MySQLdb.connect(
-         host="localhost",
-         port=3306,
-         user=username,
-         passwd=password
-         db=db_name
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
     )
 
     cursor = db.cursor()
 
-     # Execute query (only once)
-     cursor.execute("SELECT cities.id, cities.name, states.name \
-                    FROM cities JOIN states ON cities.states_id = states.id \
+    # Execute query (only once)
+    cursor.execute("SELECT cities.id, cities.name, states.name \
+                    FROM cities JOIN states ON cities.state_id = states.id \
                     ORDER BY cities.id ASC")
-     # Display results
-     for row in cursor.fetchall():
-         print(row)
-         # Close resources
-         cursor.close()
-         db.close()
 
+    # Display results
+    for row in cursor.fetchall():
+        print(row)
+
+    # Close resources
+    cursor.close()
+    db.close()
