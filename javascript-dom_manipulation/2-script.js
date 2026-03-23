ffb6d73a19ef677fb5f5fe5476135683b3aac6cd
Ryan364412 <bundhoor50@gmail.com>
2026-03-23 13:00:26 +0400

    script2

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,5 @@
+// When the  user clicks on the element with id "red_header"
+// add the class "red" to the <headr> element.
+document.querySelector('#red_header').addEventListener('click', function () {
+	document.queryselector('header').classList.add('red');
+});
