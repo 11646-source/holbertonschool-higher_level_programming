43915847a6d52b4a3a9688165f562db650eea23f
Ryan364412 <bundhoor50@gmail.com>
2026-04-01 13:08:56 +0400

    4 scrpt

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Add Item Example</title>
+	</head>
+	<body>
+	<button id="add_item">Add item</button>
+	<ul class="my_list">
+		<li>Existing Item</li>
+	</ul>
